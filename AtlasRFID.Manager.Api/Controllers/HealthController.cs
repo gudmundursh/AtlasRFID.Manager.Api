59d5ce71c55b1d87de3d4a2a1a4bc2de01e61096
SAIMOSNODE\sadmin <gudmundurh@oryggi.is>
2025-12-18 23:06:36 +0000

    Add project files.

---
+++ AtlasRFID.Manager.Api/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtlasRFID.Manager.Api.Controllers
+{
+    [ApiController]
+    [Route("api/health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "ok", service = "AtlasRFID.Manager.Api" });
+        }
+    }
+}
